25102954fe908b6f758ed12eab285bf760de526d
Kiran Anto <kirananto@gmail.com>
2021-07-18 23:10:52 +0530

    fix: Improvements in iconography

--- source/Home.tsx
+++ source/Home.tsx
@@ -19,26 +19,58 @@ export const Home = () => {
       <div className="nav-top-main">
         <div className="nav-top-container">
           <div className="nav-top-child-1">
-            <img
-              className="w-8 h-8"
-              src="./assets/icons/addProductss.svg"
-              alt=""
-            />
-            <h3 className="text-xs text-gray-600  font-semibold">
-              ADD PRODUCT
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <h3 className="text-xs text-gray-500  font-semibold">
+              Add Product
             </h3>
           </div>
           <div className="nav-top-child-2">
-            <img
-              className="w-8 h-8"
-              src="./assets/icons/allOrders.svg"
-              alt=""
-            />
-            <h3 className="text-xs text-gray-600 font-semibold">ALL ORDERS</h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+
+            <h3 className="text-xs text-gray-500 font-semibold">All Orders</h3>
           </div>
           <div className="nav-top-child-3">
-            <img className="w-8 h-8" src="./assets/icons/REPORT.svg" alt="" />
-            <h3 className="text-xs text-gray-600  font-semibold">REPORTS</h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 text-gray-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+              />
+            </svg>
+            <h3 className="text-xs text-gray-500  font-semibold">Reports</h3>
           </div>
         </div>
       </div>
@@ -67,7 +99,11 @@ export const Home = () => {
               <div className="card-main">
                 <div className="card-container">
                   <div className="card-child-1">
-                    <img className="w-8" src="./assets/icons/cardProfile.svg" alt="" />
+                    <img
+                      className="w-8"
+                      src="./assets/icons/cardProfile.svg"
+                      alt=""
+                    />
                   </div>
                   <div className="card-child-2">
                     <h2 className="user-name">Sabeer</h2>
