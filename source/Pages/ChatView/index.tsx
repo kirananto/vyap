6d7a7550014776443dc698fe6ccc7bf5310c7516
Kiran Anto <kirananto@gmail.com>
2021-09-04 23:09:13 +0530

    add logging for params
    Signed-off-by: Kiran Anto <kirananto@gmail.com>

--- source/Pages/ChatView/index.tsx
+++ source/Pages/ChatView/index.tsx
@@ -16,7 +16,10 @@ export const Payment = () => {
   const [paymentModalVisible, setPaymentModalVisible] = useState(false);
   const [inbox, setInbox] = useState<InboxType>();
   const { token } = useSelector(selectCredentials)
-  const { id } = useParams<{ id: string }>()
+  console.log('window.location.pathname', window.location)
+  const params = useParams<{ id: string }>()
+  const { id } = params
+  console.log('params', params)
   useEffect(() => {
     if(token) {
       fetchInboxById(token, id).then(res => {
