08d2ee0d43658c6fda16cc4135b9118eab925239
Kiran Anto <kirananto@gmail.com>
2021-09-06 01:02:50 +0530

    fix: fix issue with not able to access process.env

--- source/API/apiClient.ts
+++ source/API/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = process.env.SNOWPACK_PUBLIC_API_URL ?? `http://localhost:3333`
+export const baseURL = import.meta.env.SNOWPACK_PUBLIC_API_URL ?? `http://localhost:3333`
 
 export const axiosClient = axios.create({
     baseURL: `${baseURL}/api`,
