d56fe6f50d36f15240c397652c17f9cf9e0b9f1a
Ayan Ansari <1100zaid@gmail.com>
2021-09-28 09:04:43 +0530

    Added final popups & created a global popup

--- src/Pages/Employees/Employees.tsx
+++ src/Pages/Employees/Employees.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { SimpleHeader } from "../../Components/Header";
 import EmployeesCard from "./EmployeesCard";
+import Popup from "../../Components/Popup";
 
 export default function Employees() {
+  const [modalEmployee, setModalEmployee] = useState(false);
+
+  const handlePopup = () => {
+    setModalEmployee(true);
+  };
+
   return (
     <div className="w-full h-screen overflow-y-auto bg-gray-100">
       <div className="w-full mb-2 bg-white shadow">
@@ -21,8 +28,55 @@ export default function Employees() {
       {/* Footer */}
 
       <div className="fixed bottom-0 flex items-center justify-center w-full h-16 bg-white shadow">
-          <button className="w-2/4 h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700">Invite a new employee</button>
+        <button
+          onClick={handlePopup}
+          className="w-2/4 h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700"
+        >
+          Invite a new employee
+        </button>
       </div>
+      <Popup trigger={modalEmployee} setModalEmployee={setModalEmployee}>
+        <h1 className="text-2xl font-bold text-gray-500">Invite Employee</h1>
+        <div className="pt-5">
+          <p className="text-sm font-bold text-gray-500">Phone number</p>
+          <input
+            type="text"
+            placeholder="Enter your phone number... "
+            className="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-100 border border-transparent border-gray-200 rounded-lg opacity-75 focus:border-blue-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 "
+          />
+          <p className="pt-2 text-xs font-bold text-gray-300">
+            We'll never share your phone number
+            <br />
+            with anyone else
+          </p>
+
+          <div className="pt-5">
+            <p className="text-sm font-bold text-gray-500">Role</p>
+            <input
+              type="text"
+              placeholder="Order Taker/ Delivery Person"
+              className="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-100 border border-transparent border-gray-200 rounded-lg opacity-75 focus:border-blue-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 "
+            />
+          </div>
+          {/* todo:: make this multiselect inpupt */}
+          <div className="pt-5">
+            <p className="text-sm font-bold text-gray-500">
+              Assigned Customers
+            </p>
+            <input
+              type="text"
+              placeholder="Order Taker/ Delivery Person"
+              className="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-100 border border-transparent border-gray-200 rounded-lg opacity-75 focus:border-blue-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 "
+            />
+          </div>
+
+          <div className="pt-12"> 
+            <button className="w-full h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700">
+              Invite Employee
+            </button>
+          </div>
+        </div>
+      </Popup>
     </div>
   );
 }
