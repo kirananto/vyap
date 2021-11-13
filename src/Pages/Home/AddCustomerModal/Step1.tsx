2639cf698d75ba70aed529818bf9b5bb1023a8bb
djsreeraj <sreeraj011m@gmail.com>
2021-11-13 23:51:32 +0530

    added phone validation

--- src/Pages/Home/AddCustomerModal/Step1.tsx
+++ src/Pages/Home/AddCustomerModal/Step1.tsx
@@ -3,6 +3,8 @@ import { useSelector } from 'react-redux'
 import { checkIfUserExists } from 'src/API/invite.axios'
 import { selectCredentials } from 'src/Pages/Login/credentialsSlice'
 import { currentPageEnum } from '.'
+import { Length, validate } from 'class-validator';
+import { useState } from 'react';
 
 interface IProps {
   phoneNumber: string
@@ -14,6 +16,13 @@ interface IProps {
   setIsExisting: any
   isExisting: any
 }
+
+export class Post {
+  @Length(10, 10)
+  phoneNumber!: string;
+
+}
+
 export default function AddCustomerStep1({
   toggleVisibility,
   phoneNumber,
@@ -25,17 +34,32 @@ export default function AddCustomerStep1({
   isExisting
 }: IProps) {
   const { token } = useSelector(selectCredentials)
+  const [isValid, setIsValid] = useState<boolean>(true);
+
 
   const handleSubmit = () => {
-    checkIfUserExists(token!, phoneNumber).then(result => {
-      setIsExisting(true)
-      setCurrentPage(currentPageEnum.PREVIEW)
 
-    }).catch(error => {
-      setIsExisting(false)
-      setCurrentPage(currentPageEnum.NEW_CUSTOMER_STEP)
-      // TODO user doesn't exist
-    })
+    let post = new Post();
+    post.phoneNumber = phoneNumber;
+
+    validate(post).then(errors => {
+      if (errors.length > 0) {
+          console.log("validation failed. errors: ", errors);
+          setIsValid(false);
+      } else {
+          setIsValid(true);
+          console.log("validation succeed");
+          checkIfUserExists(token!, phoneNumber).then(result => {
+            setIsExisting(true)
+            setCurrentPage(currentPageEnum.PREVIEW)
+      
+          }).catch(error => {
+            setIsExisting(false)
+            setCurrentPage(currentPageEnum.NEW_CUSTOMER_STEP)
+            // TODO user doesn't exist
+          })
+      }
+    });
   }
 
   return (
@@ -51,7 +75,7 @@ export default function AddCustomerStep1({
           Phone number
         </label>
         <input
-          type="tel"
+          type="number"
           name="tel"
           value={phoneNumber}
           onChange={(event) => setPhoneNumber(event?.target.value)}
@@ -60,6 +84,10 @@ export default function AddCustomerStep1({
           className="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-200 border-transparent rounded-lg opacity-75 focus:border-blue-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2  dark:bg-gray-500 dark:text-gray-200 dark:focus:bg-gray-600"
         />
       </div>
+
+      <span className={"flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1 " + (isValid ? 'hidden' : '')}>
+			  Invalid mobile number !
+		  </span>
 
       <div className="mt-2 text-xs text-gray-400">
         We'll never share your phone number with anyone else.
@@ -71,11 +99,11 @@ export default function AddCustomerStep1({
           Opening balance
         </label>
         <input
-          type="tel"
-          name="tel"
+          type="number"
+          name="openingBalance"
           value={openingBalance}
           onChange={(event: any) => setOpeningBalance(event?.target.value)}
-          id="tel"
+          id="openingBalance"
           placeholder="Opening balance of the customer"
           className="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-200 border-transparent rounded-lg opacity-75 focus:border-blue-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2  dark:bg-gray-500 dark:text-gray-200 dark:focus:bg-gray-600"
         />
