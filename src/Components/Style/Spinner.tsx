a4e5f9e5030f6c8802a7aa9f9c29dd3d64bf9337
Kiran Anto <kirananto@gmail.com>
2021-11-14 11:12:06 +0530

    fix: fix spinners in products page

--- src/Components/Style/Spinner.tsx
+++ src/Components/Style/Spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Spinner() {
     return (
         <div className=" flex justify-center items-center">
-            <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-purple-500"></div>
+            <div className="animate-spin rounded-full h-8 w-8 border-t-4 border-b-4 border-purple-700"></div>
         </div>
     )
 }
