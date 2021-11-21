ce718181ce9c6ffbbbb90780ca9eae31ede2cd8a
Kiran Anto <kirananto@gmail.com>
2021-11-21 14:34:01 +0530

    fix: more improvements in printing from the phone

--- src/Pages/Orders/Options/PrintAll.tsx
+++ src/Pages/Orders/Options/PrintAll.tsx
@@ -5,27 +5,49 @@ import { FormattedMessage } from "react-intl";
 import { selectCredentials } from "../../Login/credentialsSlice";
 import { useSelector } from "react-redux";
 
+import ReactToPrint from "react-to-print";
+
+
 interface IProps {
   apiData: any[];
 }
 
-let txnCount : number;
+let txnCount: number;
 
 export const PrintAll = ({ apiData }: IProps) => {
   const { user } = useSelector(selectCredentials);
-  const onPrint = () => {
-    if (document.getElementById("divContents")) {
-      var printContents = document.getElementById("divContents")?.innerHTML;
-      var originalContents = document.body.innerHTML;
+  const componentRef = React.useRef(null);
 
-      if (printContents) {
-        document.body.innerHTML = printContents;
-      }
-      window.print();
-      document.body.innerHTML = originalContents;
-      window.location.reload();
-    }
-  };
+
+  const reactToPrintContent = React.useCallback(() => {
+    return componentRef.current;
+  }, [componentRef.current]);
+
+
+  const reactToPrintTrigger = React.useCallback(() => {
+    return <button
+      className="flex justify-center gap-1 items-center w-2/4 h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+        />
+      </svg>
+      <FormattedMessage
+        id="action.printAll"
+        defaultMessage="Print All"
+      />
+    </button>
+  }, [])
 
   const orders: any[] = apiData.map((item) => {
 
@@ -45,33 +67,20 @@ export const PrintAll = ({ apiData }: IProps) => {
 
   return (
     <>
-      <button
-        className="flex justify-center gap-1 items-center w-2/4 h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700"
-        onClick={onPrint}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
-          />
-        </svg>
-        <FormattedMessage
-          id="action.printAll"
-          defaultMessage="Print All"
-        />
-      </button>
+      <ReactToPrint
+        content={reactToPrintContent}
+        documentTitle={`Vyap All Orders`}
+        // onAfterPrint={handleAfterPrint}
+        // onBeforeGetContent={handleOnBeforeGetContent}
+        // onBeforePrint={handleBeforePrint}
+        removeAfterPrint
+        trigger={reactToPrintTrigger}
+      />
 
       <div
-        className="divide-y divide-gray-200  hidden grid grid-cols-1"
+        className="divide-y divide-gray-200  hidden showinprint grid grid-cols-1"
         id="divContents"
+        ref={componentRef}
       >
         <div className="grid grid-cols-5 gap-4 m-2 border-b-2 border-grey-200 py-4 px-5">
           <div className="col-start-1 col-span-1  -space-y-3 align-middle">
@@ -88,39 +97,39 @@ export const PrintAll = ({ apiData }: IProps) => {
           </div>
 
           <div className="col-end-7 col-span-1 justify-end flex flex-row align-middle border border-gray-200 p-3">
-             <img className="w-12 h-12" src={vyapLogo} />
-              <div className="flex flex-col">
-                <p className="text-xl font-bold text-gray-600"> {user?.organization?.name} </p>
-               <p className="text-gray-300">{txnCount} Transactions</p>
-             </div>
-          </div>          
+            <img className="w-12 h-12" src={vyapLogo} />
+            <div className="flex flex-col">
+              <p className="text-xl font-bold text-gray-600"> {user?.organization?.name} </p>
+              <p className="text-gray-300">{txnCount} Transactions</p>
+            </div>
+          </div>
         </div>
         <div>
-      
-        <div className="m-5 mx-10 p-5 border border-gray-200 rounded-md">
-          <table className="min-w-full">
-            <thead className="text-gray-200 font-bold">
-              <tr className="p-5">
-                <td>Order_ID</td>
-                <td>Date</td>
-                <td>Supplier</td>
-                <td>Buyer</td>
-                <td>Amount</td>
-              </tr>
-            </thead>
-            <tbody className="text-gray-500">
-              {orders.map((item, index) => (
-                <tr key={index} className="text-left ">
-                  {Object.values(item).map((val: any, i) => (
-                    <td key={val} className="px-1 py-2 whitespace-nowrap">
-                      { i==4 ? "₹"+val : val}
-                    </td>
-                  ))}
+
+          <div className="m-5 mx-10 p-5 border border-gray-200 rounded-md">
+            <table className="min-w-full">
+              <thead className="text-gray-200 font-bold">
+                <tr className="p-5">
+                  <td>Order_ID</td>
+                  <td>Date</td>
+                  <td>Supplier</td>
+                  <td>Buyer</td>
+                  <td>Amount</td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>  
+              </thead>
+              <tbody className="text-gray-500">
+                {orders.map((item, index) => (
+                  <tr key={index} className="text-left ">
+                    {Object.values(item).map((val: any, i) => (
+                      <td key={val} className="px-1 py-2 whitespace-nowrap">
+                        {i == 4 ? "₹" + val : val}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
--- src/Pages/Payments/Options/PrintAll.tsx
+++ src/Pages/Payments/Options/PrintAll.tsx
@@ -4,6 +4,7 @@ import { FormattedMessage } from "react-intl";
 import vyapLogo from "../../../assets/new_logo.svg";
 import { selectCredentials } from "../../Login/credentialsSlice";
 import { useSelector } from "react-redux";
+import ReactToPrint from "react-to-print";
 
 
 interface IProps {
@@ -13,19 +14,9 @@ interface IProps {
 let txnCount : number;
 
 export const PrintAll = ({ apiData }: IProps) => {
+
+  const componentRef = React.useRef(null);
   const { user } = useSelector(selectCredentials);
-  const onPrint = () => {
-    if (document.getElementById("divContents")) {
-      var printContents = document.getElementById("divContents")?.innerHTML;
-      var originalContents = document.body.innerHTML;
-      if (printContents) {
-        document.body.innerHTML = printContents;
-        window.print();
-      }
-      document.body.innerHTML = originalContents;
-      window.location.reload();
-    }
-  };
 
   const payments: any[] = apiData.map((item) => {
     const credit : boolean = user?.organization?.name === item.receiver?.name;
@@ -42,35 +33,61 @@ export const PrintAll = ({ apiData }: IProps) => {
     };
   });
 
+  
+  const reactToPrintContent = React.useCallback(() => {
+    return componentRef.current;
+  }, [componentRef.current]);
+
+
+  const reactToPrintTrigger = React.useCallback(() => {
+    // NOTE: could just as easily return <SomeComponent />. Do NOT pass an `onClick` prop
+    // to the root node of the returned component as it will be overwritten.
+
+    // Bad: the `onClick` here will be overwritten by `react-to-print`
+    // return <button onClick={() => alert('This will not work')}>Print this out!</button>;
+
+    // Good
+    return <button
+    className="flex justify-center gap-1 items-center w-2/4 h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700"
+    // onClick={onPrint}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
+      />
+    </svg>
+    <FormattedMessage
+      id="action.printAll"
+      defaultMessage="Print All"
+    />
+  </button>;
+  }, []);
+
   return (
     <>
-      <button
-        className="flex justify-center gap-1 items-center w-2/4 h-10 font-bold text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700"
-        onClick={onPrint}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"
-          />
-        </svg>
-        <FormattedMessage
-          id="action.printAll"
-          defaultMessage="Print All"
-        />
-      </button>
 
+<ReactToPrint
+        content={reactToPrintContent}
+        documentTitle={`Vyap All Payments`}
+        // onAfterPrint={handleAfterPrint}
+        // onBeforeGetContent={handleOnBeforeGetContent}
+        // onBeforePrint={handleBeforePrint}
+        removeAfterPrint
+        trigger={reactToPrintTrigger}
+      />
       <div
-        className="divide-y divide-gray-200  hidden grid grid-cols-1"
+        className="divide-y divide-gray-200  hidden showinprint grid grid-cols-1"
         id="divContents"
+        ref={componentRef}
       >
       <div className="grid grid-cols-5 gap-4 m-2 border-b-2 border-grey-200 py-4 px-5">
           <div className="col-start-1 col-span-1  -space-y-3 align-middle">
@@ -97,7 +114,7 @@ export const PrintAll = ({ apiData }: IProps) => {
 
         <div className="m-5 mx-10 p-5 border border-gray-200 rounded-md">
           <table className="min-w-full">
-            <thead className="text-gray-200 font-bold">
+            <thead className="text-gray-300 font-bold">
               <tr className="p-5">
               <td>Date</td>
               <td>Buyer</td>
