1398c2ad284702522cb83d48446be100e9d78526
djsreeraj <sreeraj011m@gmail.com>
2021-12-11 13:22:01 +0530

    added page reload confirmation  - data loss alert

--- src/Pages/Product/AddProduct/ProductScreen/CreateProduct.tsx
+++ src/Pages/Product/AddProduct/ProductScreen/CreateProduct.tsx
@@ -60,6 +60,14 @@ function CreateProduct() {
         }
       }
     });
+
+    //Reload confirmation alert
+    window.addEventListener("beforeunload", function (e) {
+      var confirmationMessage = "o/";
+
+      (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+      return confirmationMessage; //Webkit, Safari, Chrome
+    });
   }, []);
 
   useEffect(() => {
