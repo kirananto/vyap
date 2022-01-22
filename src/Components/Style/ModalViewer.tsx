3c041c76956bdf612a08f9d1794812e4de53c2d2
Kiran Anto <kirananto@gmail.com>
2022-01-22 17:33:08 +0530

    fix: more improvements with the popups

--- src/Components/Style/ModalViewer.tsx
+++ src/Components/Style/ModalViewer.tsx
@@ -9,7 +9,7 @@ interface IModalViewerProps {
 
 export default function ModalViewer(props: IModalViewerProps) {
     return props.isOpen ? (
-        <div className="absolute top-0 left-0 z-10">
+        <div className="fixed top-0 left-0 z-10">
             <div className="relative left-0 bg-black bg-opacity-50 h-screen w-screen">
                 <div onClick={props.onClose} className="h-screen w-screen" />
                 <div className="absolute bottom-0 rounded-t-2xl flex flex-col w-full gap-3 bg-white text-left dark:bg-gray-700 px-4 pt-4 animate__animated animate__fadeInUpBig animate__faster">
