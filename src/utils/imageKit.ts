8631af935bc84c6d10c4fa940c8ddf6d1e286a4d
Kiran Anto <kirananto@gmail.com>
2022-01-30 18:07:21 +0530

    fix: reduce image size for the icons

--- src/utils/imageKit.ts
+++ src/utils/imageKit.ts
@@ -1,7 +1,7 @@
 export const IMAGEKIT_URL = `https://ik.imagekit.io/2ts5tqew8qy`
 
 export enum IMAGEKIT_FOLDERS {
-    CENTRAL_CATALOGUE_IMAGE="CENTRAL_CATALOGUE_IMAGE"
+    CENTRAL_CATALOGUE_IMAGE='CENTRAL_CATALOGUE_IMAGE'
 }
 
-export const getImageURL = (path: string, folder: IMAGEKIT_FOLDERS) => `${IMAGEKIT_URL}/${folder}/${path}`
+export const getImageURL = (path: string, folder: IMAGEKIT_FOLDERS) => `${IMAGEKIT_URL}/${folder}/${path}?tr=w-100,h-100,dpr-2,q-60,pr-true,bg-373464`
