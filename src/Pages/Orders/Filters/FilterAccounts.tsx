240c28a388ab7facef269438f9b6cc34f9727b77
djsreeraj <sreeraj011m@gmail.com>
2022-01-31 16:45:09 +0530

    added inner scrolling for filter options

--- src/Pages/Orders/Filters/FilterAccounts.tsx
+++ src/Pages/Orders/Filters/FilterAccounts.tsx
@@ -1,79 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchInboxes } from 'src/API/inbox.axios'
-import { selectCredentials } from 'src/Pages/Login/credentialsSlice'
-import { selectOrderFilters, setAccount, setOrderStatus } from './orderFiltersSlice'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchInboxes } from "src/API/inbox.axios";
+import { selectCredentials } from "src/Pages/Login/credentialsSlice";
+import {
+  selectOrderFilters,
+  setAccount,
+  setOrderStatus,
+} from "./orderFiltersSlice";
 
 interface CategoryName {
-  item: any
-  type: 'orderStatus' | 'account'
+  item: any;
+  type: "orderStatus" | "account";
 }
 const Account = ({ item, type }: CategoryName) => {
+  const dispatch = useDispatch();
+  const filters = useSelector(selectOrderFilters);
 
-    const dispatch = useDispatch()
-    const filters = useSelector(selectOrderFilters)
+  const isChecked =
+    type === "orderStatus"
+      ? filters?.orderStatus === item.id
+      : filters?.account?.id === item?.recipient?.id;
 
-    const isChecked = type === 'orderStatus' ? filters?.orderStatus === item.id : filters?.account?.id === item?.recipient?.id
-
-    function tickCheckBox() {
-        if (type === 'orderStatus') {
-            dispatch(setOrderStatus(item?.id))
-        }
-        else {
-            dispatch(setAccount({ id: item?.recipient?.id, name: item?.recipient?.name }))
-        }
+  function tickCheckBox() {
+    if (type === "orderStatus") {
+      dispatch(setOrderStatus(item?.id));
+    } else {
+      dispatch(
+        setAccount({ id: item?.recipient?.id, name: item?.recipient?.name })
+      );
     }
+  }
 
-    return (
-        <div className="flex items-center gap-2 ml-4">
-            <input type={type === 'orderStatus' ? 'radio' : 'checkbox'} checked={isChecked} onChange={tickCheckBox} />
-            <label htmlFor="" className="text-sm font-semibold text-gray-500 dark:text-gray-400">
-                {type === 'orderStatus' ? item?.name : item?.recipient?.name}
-            </label>
-        </div>
-    )
-}
+  return (
+    <div className="ml-4 flex items-center gap-2">
+      <input
+        type={type === "orderStatus" ? "radio" : "checkbox"}
+        checked={isChecked}
+        onChange={tickCheckBox}
+      />
+      <label
+        htmlFor=""
+        className="text-sm font-semibold text-gray-500 dark:text-gray-400"
+      >
+        {type === "orderStatus" ? item?.name : item?.recipient?.name}
+      </label>
+    </div>
+  );
+};
 
 interface FilterCategories {
   heading: string;
-  type: 'orderStatus' | 'account'
+  type: "orderStatus" | "account";
 }
 export default function FilterCategory(props: FilterCategories) {
-    const [items, setItems] = useState<any[]>([])
-    const { token } = useSelector(selectCredentials)
+  const [items, setItems] = useState<any[]>([]);
+  const { token } = useSelector(selectCredentials);
 
-    useEffect(() => {
-        if (props.type === 'orderStatus') {
-            setItems([
-                {
-                    id: 'PENDING',
-                    name: 'Pending'
-                },
-                {
-                    id: 'PROCESSING',
-                    name: 'Processing'
-                },
-                {
-                    id: 'COMPLETE',
-                    name: 'Complete'
-                }
-            ])
+  useEffect(() => {
+    if (props.type === "orderStatus") {
+      setItems([
+        {
+          id: "PENDING",
+          name: "Pending",
+        },
+        {
+          id: "PROCESSING",
+          name: "Processing",
+        },
+        {
+          id: "COMPLETE",
+          name: "Complete",
+        },
+      ]);
+    } else {
+      fetchInboxes({ token: token!, limit: 100, offset: 0 }).then(
+        (result: any) => {
+          setItems(result?.data?.data?.filter((item: any) => item?.recipient));
         }
-        else {
-            fetchInboxes({ token: token!, limit: 100, offset: 0 }).then((result: any) => {
-                setItems(result?.data?.data?.filter((item: any) => item?.recipient))
-            })
-        }
-    }, [])
+      );
+    }
+  }, []);
 
-    return (
-        <div>
-            <h1 className="mb-1 text-base font-semibold text-gray-500 dark:text-gray-300">
-                {props.heading}
-            </h1>
-            <div className="flex flex-col gap-1 mt-2">
-                {items?.map(item => <Account key={item.id} item={item} type={props.type} />)}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="mb-1 text-base font-semibold text-gray-500 dark:text-gray-300">
+        {props.heading}
+      </h1>
+      <div
+        className={`mt-2 flex flex-col gap-1 ${
+          props.type === "account" && "max-h-[30vh]  overflow-y-scroll"
+        }`}
+      >
+        {items?.map((item) => (
+          <Account key={item.id} item={item} type={props.type} />
+        ))}
+      </div>
+    </div>
+  );
 }
