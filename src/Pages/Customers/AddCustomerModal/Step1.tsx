2cde259f5c86b0de038f801596ba6d20faf0c781
Kiran Anto <kirananto@gmail.com>
2022-02-17 07:27:53 +0530

    fix: change the placeholder text

--- src/Pages/Customers/AddCustomerModal/Step1.tsx
+++ src/Pages/Customers/AddCustomerModal/Step1.tsx
@@ -92,7 +92,7 @@ export default function AddCustomerStep1({
                     id="tel"
                     ref={phoneRef}
                     autoComplete="off"
-                    placeholder="Your phone number"
+                    placeholder="Customer phone number"
                     className="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform bg-gray-200 border-transparent rounded-lg opacity-75 focus:border-blue-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2  dark:bg-gray-500 dark:text-gray-200 dark:focus:bg-gray-600"
                 />
             </div>
@@ -107,7 +107,7 @@ export default function AddCustomerStep1({
             </span>
 
             <div className="mt-2 text-xs text-gray-500 dark:text-gray-400">
-        We{`'`}ll never share your phone number with anyone else.
+        We{`'`}ll never share the phone number with anyone else.
             </div>
 
             <div className="mt-4">
