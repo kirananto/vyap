4c48b1db7252ca4abdc914ae0082c3850aaf6323
Kiran Anto <kirananto@gmail.com>
2022-02-18 23:01:22 +0530

    Add Sentry Integration for bug tracking

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,19 @@ import { InitialRouter } from './App'
 import { persistor, store } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+Sentry.init({
+    dsn: 'https://ccaf8e0e8a37441a8aae84226c909553@o1147462.ingest.sentry.io/6217762',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+})
+
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
