b8f547be98145a0574b68b8b91cbe0c79f91537e
Kiran Anto <kirananto@gmail.com>
2022-03-08 09:22:42 +0530

    refactor: reuse existing components in the orders

--- src/Pages/Reports/ShopReports/PrintAll.tsx
+++ src/Pages/Reports/ShopReports/PrintAll.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import format from 'date-fns/format'
 import { FormattedMessage } from 'react-intl'
 import { selectCredentials } from '../../Login/credentialsSlice'
 import { useSelector } from 'react-redux'
@@ -9,8 +8,7 @@ import type { orderInterface } from 'src/Pages/Customers/ChatView/Cards/OrderCar
 import VyapLogo from 'src/Components/VyapLogo'
 import QRCode from 'qrcode.react'
 import Button from 'src/Components/Style/Button'
-import OrderContainerDetail from './OrderItemsDetails'
-import ReturnStatusTile from 'src/Pages/Orders/OrderStatusTag'
+import OrderContainer from 'src/Pages/Orders/OrderContainerBox'
 
 
 interface IProps {
@@ -48,23 +46,6 @@ export const PrintAll = ({ apiData }: IProps) => {
             </Button>
         )
     }, [])
-
-    const orders = apiData.map((item) => {
-        txnCount = apiData.length
-        today = item.createdAt  ? format(new Date(item.createdAt), 'dd/MM/yyyy')
-            : ''
-        return {
-            ORDER_ID: '#' + item?.id?.split('-')[0],
-            DATE: item.createdAt
-                ? format(new Date(item.createdAt), 'dd/MM/yyyy')
-                : '',
-            SUPPLIER: item?.supplier?.name,
-            BUYER: item?.buyer?.name,
-            AMOUNT: (
-                parseFloat(item?.totalAmount) - parseFloat(item?.flatDiscount)
-            ).toLocaleString('en-IN'),
-        }
-    })
 
     return (
         <>
@@ -111,62 +92,7 @@ export const PrintAll = ({ apiData }: IProps) => {
                 <div
                     className="bg-white pb-24 dark:bg-slate-800 rounded p-4 max-h-[80vh] overflow-y-scroll"
                 >
-                    {apiData.map((item, index) => (
-                        <div
-                            className={`${index === orders.length - 1 ? '' : 'border-b border-slate-300 dark:border-slate-700'
-                            }`}
-                            key={`${item.id}`}
-                        >
-                            <div className="flex flex-row mt-2 mb-3">
-                                <div>
-                                    <div className="item w-4  flex-grow-0 mr-2">
-                                        <input
-                                            className="cursor-pointer rounded border-slate-300 text-blue-800"
-                                            type="checkbox"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="w-full">
-                                    <div className="grid grid-rows-3 content-start">
-                                        <div className="grid grid-flow-col  gap-1 row-span-3">
-
-                                            <div className="text-slate-500 mt-1 text-xs dark:text-slate-300">
-                                                {item.createdAt
-                                                    ? format(new Date(item.createdAt), 'do MMM yyyy')
-                                                    : ''}
-                                            </div>
-                                        </div>
-                                        <div className="flex justify-between text-slate-600 text-sm md:text-md font-semibold dark:text-slate-200  mb-1">
-                                            {item?.buyer?.name}
-
-                                            <div className="text-slate-600  text-sm md:text-lg font-extrabold dark:text-slate-200">
-                                                {' '}
-                                                {(
-                                                    parseFloat(item?.totalAmount) -
-                                                    parseFloat(item?.flatDiscount)
-                                                ).toLocaleString('en-IN', { style: 'currency', currency: 'INR' })}
-                                            </div>
-                                        </div>
-                                        <div className="grid grid-flow-col">
-                                            <div className="col-start-1 w-max  items-center mt-1">
-                                                <ReturnStatusTile status={item?.orderStatus?.[0]?.status} />
-                                            </div>
-
-                                            <div className="text-center col-end-12 self-center text-slate-600 dark:text-slate-200 text-lg font-extrabold">
-                                            
-                                                <div className="text-slate-400 text-xs font-extrabold mx-auto dark:text-slate-300">
-                                                ({item?.numberOfItems} {item?.numberOfItems > 1 ? 'items' : 'item'})
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <OrderContainerDetail order={item} />
-
-                        </div>
-                    ))}
+                    <OrderContainer orders={apiData} expanded={true} loading={false} />
                 </div>
 
                 <div className="flex flex-row flex-wrap justify-end pt-1 mt-6 pt-2 px-5 border-t border-zinc-200">
