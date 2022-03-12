f787e2f426a4d5ffbd8304ad0a633287ca4981ee
djsreeraj <sreeraj011m@gmail.com>
2022-03-12 19:38:04 +0530

    Partial commit: order status note add

--- src/Pages/Customers/ChatView/Popups/OrderOptionsPopup.tsx
+++ src/Pages/Customers/ChatView/Popups/OrderOptionsPopup.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { hapticFeedback } from 'src/utils/vibrate'
 import { selectCredentials } from 'src/Pages/Login/credentialsSlice'
@@ -8,6 +8,7 @@ import Processing from 'src/Components/Style/Icons/Processing'
 import Pending from 'src/Components/Style/Icons/Pending'
 import { OrderStatusEnum } from 'src/Pages/Orders/enum'
 import { setOrderStatus } from '../chatListSlice'
+import { STATUS_OPTIONS } from './types'
 
 interface iProps {
     onClose: () => void,
@@ -22,6 +23,12 @@ const OrderOptionsPopup = ({ onClose, orderId, threadId, inboxId }
 
     const dispatch = useDispatch()
     const { token } = useSelector(selectCredentials)
+
+    const [statusOption, setStatusOption] = useState<STATUS_OPTIONS | undefined>(undefined)
+
+
+
+
     const handleStatusUpdate = async (statusCode: number) => {
         if (orderId && token) {
             createOrderStatus({ token: token, orderId: orderId, status: statusCode, note: `Updating status to ${OrderStatusEnum[statusCode]}` })
@@ -39,41 +46,99 @@ const OrderOptionsPopup = ({ onClose, orderId, threadId, inboxId }
     }
 
     return (
-        <div className="pb-8 pt-2 px-4">
+        <div className="pb-8 pt-2 px-2">
             {/* Heading */}
             <div className="flex items-center justify-between mb-4">
                 <h1 className="text-lg w-full font-bold text-slate-500 dark:text-slate-200">Update Order Status</h1>
             </div>
             {/* row-1 */}
             <div className="flex flex-col mt-6">
-                <button onClick={() => {
-                    handleStatusUpdate(200)
-                    hapticFeedback()
-                    onClose()
 
-                }} className="flex items-center py-3 gap-2 text-md font-semibold text-yellow-500 dark:text-yellow-300">
-                    <Pending />
-                    <span>Pending</span>
-                </button>
-                {/* ---- */}
-                <button onClick={() => {
-                    handleStatusUpdate(300)
-                    hapticFeedback()
-                    onClose()
-                }} className="flex items-center py-3 gap-2 text-md font-semibold text-blue-500 dark:text-blue-300 ">
-                    <Processing />
-                    <span>Processing</span>
-                </button>
-                {/* ---- */}
-                <button onClick={() => {
-                    handleStatusUpdate(400)
-                    hapticFeedback()
-                    onClose()
-                }} className="flex items-center py-3 gap-2 text-md font-semibold text-green-500 dark:text-green-300">
-                    <Completed />
-                    <span>Completed</span>
-                </button>
+                    <div
+                        className={
+                            `flex items-center gap-2 p-2 mt-4 text-left ${statusOption === STATUS_OPTIONS.PENDING
+                                ? 'border-2 rounded border-blue-600  dark:border-blue-400'
+                                : 'border-2 rounded border-slate-200 dark:border-slate-600'}
+                                
+                        `}
+                        onClick={() => { setStatusOption(STATUS_OPTIONS.PENDING)
+                            // handleStatusUpdate(200)
+                            // hapticFeedback()
+                            // onClose()       
+                        }}
+                    >
+                        <span className='text-yellow-500 dark:text-yellow-300'> <Pending /> </span>
+                        <span className={` ${statusOption === STATUS_OPTIONS.PENDING ? 'text-blue-700 dark:text-blue-300' : 'text-slate-700 dark:text-slate-300'}`}>Pending</span>
+
+
+                    </div>
+
+                    <div
+                        className={
+                            `flex items-center gap-2 p-2 mt-4 text-left ${statusOption === STATUS_OPTIONS.PROCESSING
+                                ? 'border-2 rounded border-blue-600  dark:border-blue-400'
+                                : 'border-2 rounded border-slate-200 dark:border-slate-600'}
+                                
+                        `}
+                        onClick={() => {    setStatusOption(STATUS_OPTIONS.PROCESSING)
+                            // handleStatusUpdate(300)
+                            // hapticFeedback()
+                            // onClose()     
+                        }}
+                    >
+
+                        <span className='text-blue-500 dark:text-blue-300'> <Processing /> </span>
+                        <span className={` ${statusOption === STATUS_OPTIONS.PROCESSING ? 'text-blue-700 dark:text-blue-300' : 'text-slate-700 dark:text-slate-300'}`} >Processing</span>
+
+
+                    </div>
+
+
+                    <div
+                        className={
+                            `flex items-center gap-2 p-2 mt-4 text-left ${statusOption === STATUS_OPTIONS.COMPLETED
+                                ? 'border-2 rounded border-blue-600  dark:border-blue-400'
+                                : 'border-2 rounded border-slate-200 dark:border-slate-600'}
+                                
+                        `}
+                        onClick={() => {  setStatusOption(STATUS_OPTIONS.COMPLETED)
+                            // handleStatusUpdate(400)
+                            // hapticFeedback()
+                            // onClose()  
+                        }}
+                    >
+
+                        <span className='text-green-500 dark:text-green-300'> <Completed /> </span>
+                        <span className={` ${statusOption === STATUS_OPTIONS.COMPLETED ? 'text-blue-700 dark:text-blue-300' : 'text-slate-700 dark:text-slate-300'}`} > Completed</span>
+
+
+                    </div>
+
+
             </div>
+
+
+            <div className="flex mt-4 pt-4 gap-2">
+                            <button
+                                onClick={() => {
+                                    hapticFeedback()
+                                    //toggleVisibility(false)
+                                }}
+                                className="active:scale-95 p-3 w-full text-indigo-700 rounded-full border border-indigo-700 dark:border-indigo-200 dark:text-indigo-200"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                onClick={() => {
+                                    // if (!loading) {
+                                    //     onConfirmOrder()
+                                    // }
+                                }}
+                                className="active:scale-95 p-3 w-full text-white rounded-full bg-gradient-to-br from-blue-500 to-indigo-700"
+                            >
+                                UPDATE
+                            </button>
+                        </div>
         </div>
     )
 }
---
+++ src/Pages/Customers/ChatView/Popups/types.ts
@@ -0,0 +1,5 @@
+export enum STATUS_OPTIONS {
+    'PENDING' = 'Pending',
+    'PROCESSING' = 'Processing',
+    'COMPLETED' = 'Completed'
+} 
