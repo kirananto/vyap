230630f142c679a4bc2c3839b75d54578f03fd93
djsreeraj <sreeraj011m@gmail.com>
2022-04-06 16:25:54 +0530

    added youtube channel link

--- src/Pages/More/More.tsx
+++ src/Pages/More/More.tsx
@@ -29,6 +29,7 @@ import { differenceInDays } from 'date-fns'
 import { clearAll as clearAllProducts } from '../Product/productsSlice'
 import { getDpImageURL, IMAGEKIT_FOLDERS } from 'src/utils/imageKit'
 import WhatsApp from 'src/Components/Style/Icons/WhatsApp'
+import Play from 'src/Components/Style/Icons/Play'
 const browser = Bowser.getParser(window.navigator.userAgent)
 export default function More() {
     const { user, token } = useSelector(selectCredentials)
@@ -121,6 +122,11 @@ export default function More() {
                     </NavLink>
                     {/* ----- */}
                     {/* ----- */}
+                    <a href="https://bit.ly/VYAP-YOUTUBE" onClick={hapticFeedback} className="flex items-center w-full gap-2 py-2 dark:text-slate-300 text-slate-500">
+                        <span className="text-indigo-500 dark:text-indigo-300 font-bold	"> <Play size={7} /></span>
+                        <Links linkName={intl.formatMessage({ id: 'global.videoTutorial' })} />
+                    </a>
+
                     <a href="https://wa.me/919037555541?text=Hello%20Team%20VYAP%20" onClick={hapticFeedback} className="flex items-center w-full gap-2 py-2 dark:text-slate-300 text-slate-500">
                         <span className="text-indigo-500 dark:text-indigo-300 font-bold	"> <WhatsApp size={7} /></span>
                         <Links linkName={intl.formatMessage({ id: 'global.whatsAppHelp' })} />
